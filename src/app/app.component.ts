d5b5f689ae626ebe5d7b4f55a7e18d367ef8d6a6
LucaSantosSP <54375485+LucaSantosSP@users.noreply.github.com>
2022-09-02 20:33:48 -0300

    Adicionado contador com Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = 'Lucas Santos';
+  nascimento = 2000;
+  counter = 0;
+  timer = setInterval(() => {
+    this.counter++;
+  }, 100);
+
+  getIdade() {
+    return 2022 - this.nascimento;
+  }
+
+  getMinuto() {
+    return Math.trunc(this.counter / 60);
+  }
+
+  getSegundos() {
+    return Math.trunc(this.counter % 60);
+  }
 }
